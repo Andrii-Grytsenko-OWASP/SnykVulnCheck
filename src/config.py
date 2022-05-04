5bc3f8ff2fd110767b2c25d7bd7f76a6593c5c7c
Andrii-Grytsenko-OWASP <90089469+Andrii-Grytsenko-OWASP@users.noreply.github.com>
2022-05-04 16:56:38 +0300

    Update config.py
    update URL parameter for API

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 class Configuration:
     SNYK_API_URL = 'https://security.snyk.io/api/'
     SNYK_API_ENDPOINT = 'listing'
-    SNYK_API_PARAM = 'q'
+    SNYK_API_PARAM = 'search'
     DATA_IN_FOLDER = "/app/data_in/"
     DATA_OUT_FOLDER = "/app/data_out/"
 
